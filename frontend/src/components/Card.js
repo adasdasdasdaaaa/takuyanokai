9175e34850dd3c8153d95c6207c4637b7bcfc947
adasdasdasdaaaa <aaaaaaaaaaoooooi@outlook.jp>
2025-10-03 23:24:04 +0900

    Delete frontend/src directory

--- frontend/src/components/Card.js
+++
@@ -1,9 +0,0 @@
-export default function Card({ message }) {
-  return (
-    <div className="bg-white shadow p-4 rounded m-2">
-      <div className="font-bold">{message.username}</div>
-      <div>{message.content}</div>
-      <div className="text-xs text-gray-400">{new Date(message.createdAt).toLocaleString()}</div>
-    </div>
-  );
-}
